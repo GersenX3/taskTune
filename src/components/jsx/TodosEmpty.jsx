c24128a00762c49a27219399c71086c6b4f78fcb
GersenX3 <galdl1438@gmail.com>
2023-06-19 14:02:52 -0600

    Pantalla sin todos estilisada

--- src/components/jsx/TodosEmpty.jsx
+++ src/components/jsx/TodosEmpty.jsx
@@ -1,8 +1,15 @@
 import React from 'react'
+import { Engines } from '../svg/Engines'
+import '../css/TodosEmpty.css'
 
 function TodosEmpty() {
   return (
-    <div>TodosEmpty</div>
+    <div className='container'>
+      <p>Create your first Task!!</p>
+      <div className='engines'>
+      <Engines/>
+      </div>
+    </div>
   )
 }
 export {TodosEmpty}
